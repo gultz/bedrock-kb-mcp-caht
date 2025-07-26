4004177c449e049761333a68ca717ab8f78ab0e5
gultz <gultz5102@kakao.com>
2025-07-26 15:15:53 +0900

    fix: chat session

--- pages/2_MCP_chembl_agent.py
+++ pages/2_MCP_chembl_agent.py
@@ -11,23 +11,23 @@ if not logger.hasHandlers():
     logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
-st.title("MCP Page")
-st.write("This is the MCP page content.")
+st.title("CHEMBL_MCP Page")
+st.write("This is the CHEMBL_MCP page content.")
 
-if "mcp_messages" not in st.session_state:
-    st.session_state["mcp_messages"] = [
+if "CHEMBL_MCP_messages" not in st.session_state:
+    st.session_state["CHEMBL_MCP_messages"] = [
         {"role": "assistant", "content": "안녕하세요, 무엇이 궁금하세요?"}
     ]
 # 지난 답변 출력
-for msg in st.session_state.mcp_messages:
+for msg in st.session_state.CHEMBL_MCP_messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 
 #유저가 쓴 chat을 query라는 변수에 담음
-query = st.chat_input("chat with MCP")
+query = st.chat_input("chat with CHEMBL_MCP_messages")
 if query:
     # Session에 메세지 저장
-    st.session_state.mcp_messages.append({"role": "user", "content": query})
+    st.session_state.CHEMBL_MCP_messages.append({"role": "user", "content": query})
     
     # UI에 출력
     st.chat_message("user").write(query)
@@ -37,4 +37,4 @@ if query:
     st.chat_message("assistant").write(answer)
 
     # Session 메세지 저장
-    st.session_state.mcp_messages.append({"role": "assistant", "content": answer})
+    st.session_state.CHEMBL_MCP_messages.append({"role": "assistant", "content": answer})
--- pages/4_MCP_Opentargets.py
+++ pages/4_MCP_Opentargets.py
@@ -3,7 +3,7 @@ import mcp_agent
 import logging
 import sys
 
-logger = logging.getLogger("MCP")  # 예: "MCP" 또는 "KB"
+logger = logging.getLogger("OpenTargets_MCP")  # 예: "MCP" 또는 "KB"
 if not logger.hasHandlers():
     handler = logging.StreamHandler(sys.stderr)
     formatter = logging.Formatter('%(filename)s:%(lineno)d | %(message)s')
@@ -11,15 +11,15 @@ if not logger.hasHandlers():
     logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
-st.title("MCP Page")
-st.write("This is the MCP page content.")
+st.title("OpenTargets_MCP Page")
+st.write("This is the OpenTargets_MCP page content.")
 
-if "mcp_messages" not in st.session_state:
-    st.session_state["mcp_messages"] = [
+if "OpenTargets_mcp_messages" not in st.session_state:
+    st.session_state["OpenTargets_mcp_messages"] = [
         {"role": "assistant", "content": "안녕하세요, 무엇이 궁금하세요?"}
     ]
 # 지난 답변 출력
-for msg in st.session_state.mcp_messages:
+for msg in st.session_state.OpenTargets_mcp_messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 
@@ -27,7 +27,7 @@ for msg in st.session_state.mcp_messages:
 query = st.chat_input("chat with MCP")
 if query:
     # Session에 메세지 저장
-    st.session_state.mcp_messages.append({"role": "user", "content": query})
+    st.session_state.OpenTargets_mcp_messages.append({"role": "user", "content": query})
     
     # UI에 출력
     st.chat_message("user").write(query)
@@ -37,4 +37,4 @@ if query:
     st.chat_message("assistant").write(answer)
 
     # Session 메세지 저장
-    st.session_state.mcp_messages.append({"role": "assistant", "content": answer})
+    st.session_state.OpenTargets_mcp_messages.append({"role": "assistant", "content": answer})
