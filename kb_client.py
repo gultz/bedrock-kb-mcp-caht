14391745e9685f9b498d053c3dce13716d0b46d7
gultz <gultz5102@kakao.com>
2025-08-06 11:08:56 +0900

    update:kb

--- kb_client.py
+++ kb_client.py
@@ -7,18 +7,18 @@ region_name = session.region_name or "us-west-2"
 ssm = session.client('ssm', region_name=region_name)
 bedrock_agent_runtime_client = session.client("bedrock-agent-runtime", region_name="us-west-2")
 
-def get_knowledge_base_id(key, enc=False):
-    if enc: WithDecryption = True
-    else: WithDecryption = False
-    response = ssm.get_parameters(
-        Names=[key,],
-        WithDecryption=WithDecryption
-    )
-    return response['Parameters'][0]['Value']
+# def get_knowledge_base_id(key, enc=False):
+#     if enc: WithDecryption = True
+#     else: WithDecryption = False
+#     response = ssm.get_parameters(
+#         Names=[key,],
+#         WithDecryption=WithDecryption
+#     )
+#     return response['Parameters'][0]['Value']
 
-MODEL_ARN = "arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-5-sonnet-20241022-v2:0"
+MODEL_ARN = "arn:aws:bedrock:us-west-2:170483442401:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0"
 # KNOWLEDGE_BASE_ID = get_knowledge_base_id(key="/RAGChatBot/KNOWLEDGE_BASE_ID", enc=False)
-KNOWLEDGE_BASE_ID = "5TEUZRFQYT"
+KNOWLEDGE_BASE_ID = "UUKNDSQHHQ"
 
 def query(question):
     response = bedrock_agent_runtime_client.retrieve_and_generate(
